8becf950bccb030fda361490998b7d9ed3c18551
VanNam123 <phamvannamttll@gmail.com>
2017-06-26 10:58:49 -0700

    Chiều dài rắn và độ rộng ma trận\

---
+++ MyStaticVar.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gameconran;
+
+/**
+ *
+ * @author NAM
+ */
+public class MyStaticVar {
+    public static int X = 500;        // kích thước x của Frame
+    public static int Y = 300;        // kích thước y của Frame
+    public static int sizeCell = 8;  // kích thước cạnh của cell
+    public static int maxLengthSnake = 100; // chiều dài tối đa của snake
+}
